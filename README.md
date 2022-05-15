36b0abfe802773d646a45e67634889788128740f
cassidytrbl <97814148+cassidytrbl@users.noreply.github.com>
2022-05-14 19:44:17 -0700

    adding the index.js

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # note-taker
+
+## Table of contents
+
+- [Description](#description)
+
+- [Installation](#installation)
+
+- [Usage](#usage)
+
+- [Tests](#tests)
+
+- [Questions](#questions)
+
+## Description
+
+Application allows the user to write and save notes throughout the day, so they can keep track of their tasks.
+
+## Installation
+
+## Usage
+
+## Tests
+
+## Questions
---
+++ index.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const fullNotes = require("./db/db.json");
+const { v4: uuidv4 } = require("uuid");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+app.get("/api/notes", (req, res) => {
+  res.json(fullNotes.slice(1));
+});
+
+app.post("/api/notes", (req, res) => {
+  const { title, text } = req.body;
+  const createdNotes = { title: title, text: text, id: uuidv4 };
+  const storedData = fs.readFileSync("./db/db.json", "utf-8");
+  const formattedNotes = [].concat(JSON.parse(storedData));
+  const newNotes = [...formattedNotes, createdNotes];
+  fs.writeFileSync("db/db.json", JSON.stringify(newNotes));
+  res.json(newNotes);
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.listen(PORT, () => console.log(`listening on PORT: ${PORT}`));
